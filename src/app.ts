e2e8433a05c39dbfd969a385e708b075b2076998
gittores <jat055000@gmail.com>
2021-02-24 22:51:53 -0600

    created Network and Hub classes

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,5 @@
 import { Hub } from "./utils";
 import { sample_nodes } from "./utils";
-import * as zmq from "zeromq";
-import readline from "readline";
-
-// // socket to talk to clients
-// const requestor = new zmq.Request();
-
-// const sendMsg = async (msg: string): Promise<void> => {
-//   await requestor.send(msg);
-// };
-
-// const ping = async (workerbee: WorkerBee) => {
-//   await workerbee.requestor.send(JSON.stringify({ id: workerbee.id, request: "ping" }));
-//   let msgs: string[] = [];
-//   for await (const reply of requestor) {
-//     msgs.push(JSON.stringify(reply));
-//   }
-// };
-
-// const manage = async (workerbees: WorkerBee[]) => {
-//   console.log("Cortana initiated...checking worker states");
-
-//   //let requestors: {id: number, requestor: zmq.Request}[] = [];
-//   workerbees.forEach((workerbee) => {
-//     //get requestor
-//     workerbee.requestor = new zmq.Request();
-
-//     //connect requestor
-//     workerbee.requestor.connect(workerbee.socket);
-
-//     //get requestor uptime
-//     workerbee.ping = ping(workerbee);
-//   });
-
-//   console.log(workerbees);
-// };
-
-// manage(workerbees);
-
-// process.on("SIGINT", function () {
-//   // requestor.disconnect("tcp://127.0.0.1:1234");
-//   requestor.close();
-// });
 
 (async () => {
   function terminate(): void {
@@ -51,9 +9,13 @@ import readline from "readline";
     process.exit(0);
   }
 
-  const cortana = new Hub(sample_nodes);
+  const cortana = new Hub("main controller", sample_nodes);
   cortana.connect();
   console.log(await cortana.status());
 
   setInterval(() => {}, 1 << 30);
+
+  process.on("SIGINT", function () {
+    terminate();
+  });
 })();
